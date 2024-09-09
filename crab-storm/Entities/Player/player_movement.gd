aff0c26762cbabc1240fdf312fa82993c07cf483
Antoni Strasz <77853765+Antonekk@users.noreply.github.com>
2024-09-09 22:02:51 +0200

    8-direction movement added
    + Vector normalization
    + New Character Sprite

--- crab-storm/Entities/Player/player_movement.gd
+++ crab-storm/Entities/Player/player_movement.gd
@@ -1,22 +1,36 @@
 extends CharacterBody2D
 
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 40.0
+const RUN_SPEED = 60.0
+
+@onready var player_sprite: Sprite2D = $Sprite2D
 
 
 func _physics_process(delta: float) -> void:
 	
-	var direction_vertical := Input.get_axis("ui_left", "ui_right")
-	var direction_horizontal := Input.get_axis("ui_up", "ui_down")
-	if direction_vertical:
-		velocity.x = direction_vertical * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	if direction_horizontal:
-		velocity.y = direction_horizontal * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+	# Get input direction -1, 0, 1 depending on input
+	var direction_horizontal := Input.get_axis("Move_Left", "Move_Right")
+	var direction_vertical := Input.get_axis("Move_Up", "Move_Down")
+	var is_running := Input.is_action_pressed("Run")
 
+	
+	 # Create a direction vector from the input
+	var direction2D = Vector2(direction_horizontal, direction_vertical)
+	
+	# Normalize the direction vector if it's not zero
+	if direction2D.length() > 0:
+		direction2D = direction2D.normalized()
+	
+	# Set velocity based on the normalized direction
+	var movement_multiplier = SPEED if !is_running else RUN_SPEED
+	velocity = direction2D * movement_multiplier
+	
+	
+
+	# Handle sprite fliping
+	if direction_horizontal != 0:
+		player_sprite.flip_h = direction_horizontal > 0
+
+	# Move the player using the velocity
 	move_and_slide()
