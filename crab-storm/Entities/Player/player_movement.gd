8a00e404bdb4d4e644b34e2a3ba08962df52c56b
Kacper Pajor <61703350+kapsio123@users.noreply.github.com>
2024-09-12 23:07:30 +0200

    Revert "a"

--- crab-storm/Entities/Player/player_movement.gd
+++ crab-storm/Entities/Player/player_movement.gd
@@ -10,18 +10,10 @@ const DODGE_SPEED: float = 200.0
 const DODGE_COOLDOWN: float = 5.0
 const DODGE_TIME: float = 0.2
 
-const COLLECT_AMMO_TIME: float = 0.5
-
-
-
 
 @onready var picker: Picker = $Picker
 
 enum DodgeState {CAN_DODGE, IS_DODGING ,DODGE_ON_COOLDOWN}
-
-enum AttackState {EMPTY_AMMO,COLLECTING_AMMO ,HAS_AMMO}
-
-var CurrentAttackState : AttackState = AttackState.EMPTY_AMMO
 
 var CurrentDodgingState : DodgeState = DodgeState.CAN_DODGE
 
@@ -40,17 +32,10 @@ func handle_dodge_cooldowns() -> void:
 	print("DODGE IS UP")
 	CurrentDodgingState = DodgeState.CAN_DODGE
 	
-func handle_ammo_pickup() -> void:
-	CurrentAttackState = AttackState.COLLECTING_AMMO
-	print("COLLECTING")
-	await get_tree().create_timer(COLLECT_AMMO_TIME).timeout 
-	print("COLLECTED")
-	CurrentAttackState = AttackState.HAS_AMMO
 	
 	
-func handle_shooting_projectile() -> void:
-	CurrentAttackState = AttackState.EMPTY_AMMO
-	print("Shoot")
+	
+
 
 func _physics_process(delta: float) -> void:
 	
@@ -59,44 +44,20 @@ func _physics_process(delta: float) -> void:
 	var direction_vertical := Input.get_axis("Move_Up", "Move_Down")
 	var is_running := Input.is_action_pressed("Run")
 	var dodge_input := Input.is_action_pressed("Dodge")
-	var pickup_input := Input.is_action_just_pressed("Interact")
-	var shoot_input := Input.is_action_just_pressed("Shoot")
+	var pickup_input := Input.is_action_just_pressed("interact")
 	
-	#handle shooting logic
-	if shoot_input and CurrentAttackState == AttackState.EMPTY_AMMO:
-		handle_ammo_pickup()	
-	elif shoot_input and CurrentAttackState == AttackState.HAS_AMMO:
-		handle_shooting_projectile()
-	elif CurrentAttackState == AttackState.COLLECTING_AMMO:
-		velocity = Vector2(0,0)
+if dodge_input and CurrentDodgingState == DodgeState.CAN_DODGE:
+		dodge_direction = direction2D
+		handle_dodge_cooldowns()
+		
+	if CurrentDodgingState == DodgeState.IS_DODGING:
+		velocity = dodge_direction * DODGE_SPEED
 	else :
-		if pickup_input:
-			if picker.CurrentPickerState == picker.PickerState.Empty:
-				picker.pickup()
-			else:
-				picker.throw()
-		 # Create a direction vector from the input
-		var direction2D := Vector2(direction_horizontal, direction_vertical)
-		
-		# Normalize the direction vector if it's not zero
-		if direction2D.length() > 0:
-			direction2D = direction2D.normalized()
-		
-		# Handle sprite fliping
-		if direction_horizontal != 0:
-			player_sprite.flip_h = direction_horizontal > 0
-			
-		if dodge_input and CurrentDodgingState == DodgeState.CAN_DODGE:
-			dodge_direction = direction2D
-			handle_dodge_cooldowns()
-			
-		if CurrentDodgingState == DodgeState.IS_DODGING:
-			velocity = dodge_direction * DODGE_SPEED
-		else :
-			# Set velocity based on the normalized direction
-			var movement_multiplier := SPEED if !is_running else RUN_SPEED
-			velocity = direction2D * movement_multiplier
+		# Set velocity based on the normalized direction
+		var movement_multiplier := SPEED if !is_running else RUN_SPEED
+		velocity = direction2D * movement_multiplier
 	
 		
+
 	# Move the player using the velocity
 	move_and_slide()
