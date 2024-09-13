bd139cac791767f0c4dd33d3ef6b8def07202a1d
Antoni Strasz <77853765+Antonekk@users.noreply.github.com>
2024-09-13 19:25:07 +0200

    Movement re-write and animations

--- crab-storm/Entities/Player/player_movement.gd
+++ crab-storm/Entities/Player/player_movement.gd
@@ -20,13 +20,16 @@ var Rock_Ammo_Projectile = preload("res://Entities/Projectiles/Rock_Projectile/p
 @onready var picker: Picker = $Picker
 @onready var bullet_spawn_point: Marker2D = $BulletSpawnPoint
 
+enum MovementState {IDLE, WALKING, RUNNING}
+var CurrentMovementState : MovementState = MovementState.IDLE
+
 enum DodgeState {CAN_DODGE, IS_DODGING ,DODGE_ON_COOLDOWN}
+var CurrentDodgingState : DodgeState = DodgeState.CAN_DODGE
 
 enum AttackState {EMPTY_AMMO,COLLECTING_AMMO ,HAS_AMMO}
-
 var CurrentAttackState : AttackState = AttackState.EMPTY_AMMO
 
-var CurrentDodgingState : DodgeState = DodgeState.CAN_DODGE
+
 
 var dodge_direction:Vector2
 
@@ -62,30 +65,20 @@ func handle_shooting_projectile() -> void:
 	ammo_instance.apply_impulse(vect*BULLET_SPEED, Vector2(0,0))
 	get_tree().root.add_child(ammo_instance)
 	
-
-func _physics_process(delta: float) -> void:
-	# Get input direction -1, 0, 1 depending on input
-	var direction_horizontal := Input.get_axis("Move_Left", "Move_Right")
-	var direction_vertical := Input.get_axis("Move_Up", "Move_Down")
-	var is_running := Input.is_action_pressed("Run")
-	var dodge_input := Input.is_action_pressed("Dodge")
-	var pickup_input := Input.is_action_just_pressed("Interact")
-	var shoot_input := Input.is_action_just_pressed("Shoot")
 	
-	#handle shooting logic
-	if shoot_input and CurrentAttackState == AttackState.EMPTY_AMMO:
-		handle_ammo_pickup()	
-	elif shoot_input and CurrentAttackState == AttackState.HAS_AMMO:
-		handle_shooting_projectile()
-	elif CurrentAttackState == AttackState.COLLECTING_AMMO:
+#handle player movement
+func movement() -> void:
+	
+	if CurrentAttackState == AttackState.COLLECTING_AMMO:
 		velocity = Vector2(0,0)
-	else :
-		if pickup_input:
-			if picker.CurrentPickerState == picker.PickerState.Empty:
-				picker.pickup()
-			else:
-				picker.throw()
-		 # Create a direction vector from the input
+	else:
+		var is_running := Input.is_action_pressed("Run")
+		var dodge_input := Input.is_action_pressed("Dodge")
+		# Get input direction -1, 0, 1 depending on input
+		var direction_horizontal := Input.get_axis("Move_Left", "Move_Right")
+		var direction_vertical := Input.get_axis("Move_Up", "Move_Down")
+		
+		#Create direction vector
 		var direction2D := Vector2(direction_horizontal, direction_vertical)
 		
 		# Normalize the direction vector if it's not zero
@@ -94,9 +87,9 @@ func _physics_process(delta: float) -> void:
 		
 		# Handle sprite fliping
 		if direction_horizontal != 0:
-			player_sprite.flip_h = direction_horizontal > 0
-			
-		if dodge_input and CurrentDodgingState == DodgeState.CAN_DODGE:
+				player_sprite.flip_h = direction_horizontal > 0
+				
+		if dodge_input and CurrentDodgingState == DodgeState.CAN_DODGE and picker.CurrentPickerState == picker.PickerState.Empty and CurrentAttackState != AttackState.COLLECTING_AMMO:
 			dodge_direction = direction2D
 			handle_dodge_cooldowns()
 			
@@ -107,6 +100,47 @@ func _physics_process(delta: float) -> void:
 			var movement_multiplier := SPEED if !is_running else RUN_SPEED
 			velocity = direction2D * movement_multiplier
 	
-		
+		if direction2D == Vector2.ZERO:
+			CurrentMovementState = MovementState.IDLE
+		elif is_running:
+			CurrentMovementState = MovementState.RUNNING
+		else:
+			CurrentMovementState = MovementState.WALKING
+
+func pickup() -> void:
+	var pickup_input := Input.is_action_just_pressed("Interact")
+	if pickup_input:
+		if picker.CurrentPickerState == picker.PickerState.Empty and CurrentAttackState != AttackState.COLLECTING_AMMO and CurrentDodgingState!= DodgeState.IS_DODGING:
+			picker.pickup()
+		elif picker.CurrentPickerState == picker.PickerState.Picked :
+			picker.throw()
+	
+func shooting() -> void:
+	var shoot_input := Input.is_action_just_pressed("Shoot")
+	#handle shooting logic
+	if picker.CurrentPickerState == picker.PickerState.Empty and CurrentDodgingState!= DodgeState.IS_DODGING:
+		if shoot_input and CurrentAttackState == AttackState.EMPTY_AMMO:
+			handle_ammo_pickup()	
+		elif shoot_input and CurrentAttackState == AttackState.HAS_AMMO:
+			handle_shooting_projectile()
+
+
+func _physics_process(delta: float) -> void:
+	
+	shooting()
+	pickup()
+	movement()
+	
+	var current_animation = player_sprite.animation
+	if CurrentDodgingState != DodgeState.IS_DODGING:
+		if CurrentMovementState == MovementState.WALKING and current_animation != "walk":
+			player_sprite.play("walk")
+		if CurrentMovementState == MovementState.IDLE and current_animation != "idle":
+			player_sprite.play("idle")
+		if CurrentMovementState == MovementState.RUNNING and current_animation != "run":
+			player_sprite.play("run")
+	elif CurrentDodgingState == DodgeState.IS_DODGING and current_animation != "dodge":
+		player_sprite.play("dodge")
+	
 	# Move the player using the velocity
 	move_and_slide()
