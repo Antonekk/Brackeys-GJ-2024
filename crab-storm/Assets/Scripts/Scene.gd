b9dfe0106391776b093814aff4206478c30db2c7
Kacper Pajor <1dagon1@wp.pl>
2024-09-13 23:04:49 +0200

    not tested

--- crab-storm/Assets/Scripts/Scene.gd
+++ crab-storm/Assets/Scripts/Scene.gd
@@ -1,5 +1,15 @@
 extends Node2D
 
+var parameteres = {
+	"wood": 0,
+	"stone": 0,
+	"fish": 0,
+	"player_pos": null,
+	"time": null
+}
 
 func cleanup():
 	queue_free()
+
+func load_level_data(new: Dictionary):
+	parameteres = new
