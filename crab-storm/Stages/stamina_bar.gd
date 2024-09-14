fbca53998c1eb29e64f6259a8b6170caf46793c0
Antoni Strasz <77853765+Antonekk@users.noreply.github.com>
2024-09-14 22:09:48 +0200

    stage

---
+++ crab-storm/Stages/stamina_bar.gd
@@ -0,0 +1,13 @@
+extends TextureProgressBar
+
+
+@export var player : PlayerController
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	player.health_system.health_change.connect(update)
+	update()
+
+func update() -> void:
+	value = (player.health_system.current_health * 100) / player.health_system.MAX_HEALTH
