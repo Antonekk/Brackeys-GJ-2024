7aff4045b98f033d16971e24153fc942d77e6ba4
Kacper Pajor <1dagon1@wp.pl>
2024-09-14 23:13:29 +0200

    nwm

--- crab-storm/Stages/Minigames/kilof.gd
+++ crab-storm/Stages/Minigames/kilof.gd
@@ -1,7 +1,16 @@
 extends AnimatedSprite2D
 
+var enabled = true
+
+func _ready() -> void:
+	SignalBus.level_change.connect(Disable)
+	
+func Disable(temp):
+	stop()
+	enabled = false
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("lmb") or Input.is_action_just_pressed("rmb"):
-		stop()
-		play("kilof")
+	if enabled:
+		if Input.is_action_just_pressed("lmb") or Input.is_action_just_pressed("rmb"):
+			stop()
+			play("kilof")
